46968668713b2045ef7084aa2d7d0283e6cecfd5
David Ayres <ayresdavid29@hotmail.com>
2023-08-07 21:48:50 -0300

    Department and Seller entities class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 	
 	public static void main(String[] args) {
 	
-		Department dp = new Department(1, "David");
+		Department dp = new Department(1, "Livros");
 		
-		System.out.println(dp);
+		Seller seller = new Seller(1, "David", "ayresdavid29@hotmail.com", new Date(), 3000.0, dp);
+		
+		System.out.println(seller);
 		
 	}
 }
