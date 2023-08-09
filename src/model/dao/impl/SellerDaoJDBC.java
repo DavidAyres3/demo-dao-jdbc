426c0192cb8fd81a168f4daa6d2055e20385d550
David Ayres <ayresdavid29@hotmail.com>
2023-08-09 01:19:44 -0300

    update implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -71,7 +71,31 @@ public class SellerDaoJDBC implements SellerDao{
 
 	@Override
 	public void update(Seller obj) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		
+		try {
+			//Método para atualizar os valores da tabela seller.
+			st = conn.prepareStatement("UPDATE seller "
+					+ "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
+					+ "WHERE Id = ?");
+			
+	        st.setString(1, obj.getName());
+	        st.setString(2, obj.getEmail());
+	        st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+	        st.setDouble(4, obj.getBaseSalary());
+	        st.setInt(5, obj.getDepartment().getId());
+	        st.setInt(6, obj.getId());
+
+			st.executeUpdate();
+			
+	    } catch (SQLException e) {
+	        // Capturando e relançando qualquer exceção SQL como uma exceção personalizada
+	        throw new DbException(e.getMessage());
+	    } finally {
+	        // Certificando-se de que o PreparedStatement seja fechado, independentemente do resultado
+	        DB.closeStatement(st);
+	    }
+	
 		
 	}
 
